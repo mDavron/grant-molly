9347a8c3a153a52531400b5a43dd4177ede6a607
Davron01~ <d.mirabdullayev@mail.ru>
2023-08-02 11:04:11 +0500

    home pagedagi banner text responsive qilish

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,21 +5,23 @@ import React from 'react'
 const AboutPage = () => {
   return (
     <section className=''>
-      <div className='relative flex h-[60vh] w-full justify-center bg-[url("/images/baner4.jpg")] bg-cover bg-fixed lg:h-screen'>
+      <div className='relative flex h-[80vh]  w-full justify-center bg-[url("/images/baner4.jpg")] bg-cover bg-fixed lg:h-screen'>
         <div className='absolute left-0 top-0  h-full w-full bg-gray-800/70'>
-          <div className='container mx-auto mt-[250px]'>
-            <h1 className='text-6xl font-semibold uppercase text-white '>
-              <span className='mb-6 block text-red-500'>МОТОРНОЕ МАСЛО</span>
+          <div className='container mx-auto mt-[150px] lg:mt-[250px]'>
+            <h1 className='text-3xl font-semibold uppercase text-white lg:text-6xl '>
+              <span className='mb-2 block text-red-500 lg:mb-6'>
+                МОТОРНОЕ МАСЛО
+              </span>
               для автомобилей
             </h1>
-            <p className='mb-10 mt-6 w-1/2 text-xl font-semibold text-white'>
+            <p className='mb-10 mt-6  text-xl font-semibold text-white lg:w-1/2'>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat
               dolores corrupti nemo et nesciunt odit quidem debitis modi culpa
               velit!
             </p>
             <Link
               href={'/'}
-              className='rounded-md bg-red-500/80 px-8 py-4 text-lg font-semibold uppercase text-white'
+              className='rounded-md bg-red-500/80 px-5 py-3 text-base font-semibold uppercase text-white transition hover:bg-red-500 md:px-8 md:py-4 md:text-lg'
             >
               lorem ipsum
             </Link>
