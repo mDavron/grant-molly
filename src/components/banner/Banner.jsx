cfae2c8224bed8798ca202f25087d6130db63388
Davron01~ <d.mirabdullayev@mail.ru>
2023-08-02 19:17:00 +0500

    test banner section

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section>
+      <div className='relative flex h-screen w-full justify-center bg-[url("/images/baner4.jpg")] bg-cover bg-fixed'>
+        <div className='absolute left-0 top-0  h-full w-full bg-gray-800/70'>
+          <div className='container mx-auto mt-[150px] lg:mt-[250px]'>
+            <h1 className='text-3xl font-semibold uppercase text-white lg:text-6xl '>
+              <span className='mb-2 block text-red-500 lg:mb-6'>
+                МОТОРНОЕ МАСЛО
+              </span>
+              для автомобилей
+            </h1>
+            <p className='mb-10 mt-6  text-xl font-semibold text-white lg:w-1/2'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat
+              dolores corrupti nemo et nesciunt odit quidem debitis modi culpa
+              velit!
+            </p>
+            <Link
+              href={'/'}
+              className='rounded-md bg-red-500/80 px-5 py-3 text-base font-semibold uppercase text-white transition hover:bg-red-500 md:px-8 md:py-4 md:text-lg'
+            >
+              КАТАЛОГ МАСЛА
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Banner
