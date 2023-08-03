148d5d3c851d4eddc692ed42822f3d1b942e7fc3
Davron01~ <d.mirabdullayev@mail.ru>
2023-08-03 23:42:55 +0500

    info

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -22,10 +22,22 @@ const Banner = () => {
               </span>
               для автомобилей
             </h1>
-            <p className='mb-10 mt-6  text-lg font-semibold text-white lg:w-1/2 lg:text-xl'>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat
-              dolores corrupti nemo et nesciunt odit quidem debitis modi culpa
-              velit!
+            <p className='mb-10 mt-6  flex items-center  gap-4 text-lg font-semibold text-yellow-500 lg:w-1/2 lg:text-xl'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='h-6 w-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z'
+                />
+              </svg>
+              Сайт находится в тестовом режиме!
             </p>
             <Link
               href={'/'}
